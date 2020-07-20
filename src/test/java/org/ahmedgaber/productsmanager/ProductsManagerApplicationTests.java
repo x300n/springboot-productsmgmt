97b864f0c8dbb3a11d5febd649e1a75a74855bad
x300n <houseofxeon@gmail.com>
2020-07-20 20:37:05 +0200

    Initialize

---
+++ src/test/java/org/ahmedgaber/productsmanager/ProductsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ahmedgaber.productsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
